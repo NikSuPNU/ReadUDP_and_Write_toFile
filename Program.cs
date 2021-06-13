01ea8fb514116876d3d8e844592ce13899bcc227
NikSuPNU <niksu,831@gmail.com>
2021-06-14 03:19:48 +1000

    Первые файлы проекта, пока что изменений нет

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using LookSin;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace Recive_data_from_ADC
+{
+    class Program
+    {
+        static bool exitSystem = false;
+        static TOP_file tOP_File_2;
+        static TOP_file tOP_File;
+
+
+
+        #region Trap application termination
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler _handler;
+
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig)
+        {
+            Console.WriteLine("Exiting system due to external CTRL-C, or process kill, or shutdown");
+
+
+
+
+
+            ////do your cleanup here
+            //System.Threading.Thread.Sleep(5000); //simulate some cleanup delay
+
+            //Console.WriteLine("Cleanup complete");
+
+            ////allow main to run off
+            //exitSystem = true;
+
+            ////shutdown right away so there are no lingering threads
+            Environment.Exit(-1);
+
+            return true;
+        }
+        #endregion
+
+
+        static void Main(string[] args)
+        {
+            // Some biolerplate to react to close window event, CTRL-C, kill, etc
+            _handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(_handler, true);
+
+
+
+            byte[] ip = {192,168,0,57};
+            IPAddress iPAddres = new IPAddress(ip);
+            tOP_File = new TOP_file($@"C:\Users\Nikita\Desktop\LOL", iPAddres, 1400);
+            Console.WriteLine("Запись с порта 1400 начата");
+            tOP_File.StartRecive();
+
+
+            tOP_File_2 = new TOP_file($@"C:\Users\Nikita\Desktop\LOL", iPAddres, 2500);
+            tOP_File_2.StartRecive();
+            Console.WriteLine("Запись с порта 2500 начата");
+            
+        }
+
+    }
+}
---
+++ TOP_file.cs
@@ -0,0 +1,91 @@
+﻿using LookSin;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Recive_data_from_ADC
+{
+    class TOP_file
+    {
+        /// <summary>
+        /// Порт для прослушивания
+        /// </summary>
+        private static uint NumPort;
+        public static uint numPort { get { return NumPort; } private set { } }
+
+
+
+
+        /// <summary>
+        /// Путь к катологу для прослушивания
+        /// </summary>
+        private static string path_to_catolog_for_file;
+        public static string Path_to_catolog_for_file { get { return path_to_catolog_for_file; } private set { } }
+
+
+
+        /// <summary>
+        /// ip адрес отправителя
+        /// </summary>
+        private static IPAddress iPAddress;
+        public static IPAddress IPAdd_ { get { return iPAddress; } private set { } }
+
+        public static UDHHandler UDPhandler;
+
+        public static WriterIn_FILE WR_File;
+
+
+        Thread thread_WR;
+        Thread thread_UDP;
+
+        public TOP_file(string path_to_catolog_for_file, IPAddress iPAddress, uint NumPort)
+        {
+
+            ////////////////////////////////////////////////////////////////
+            UDPhandler = new UDHHandler(NumPort, iPAddress);
+            UDPhandler.DataRecive += UDPhandler_DataRecive;
+
+            ////////////////////////////////////////////////////////////////
+            WR_File = new WriterIn_FILE(NumPort, path_to_catolog_for_file);
+            TOP_file.NumPort = NumPort;
+            TOP_file.path_to_catolog_for_file = path_to_catolog_for_file;
+            TOP_file.iPAddress = iPAddress;
+
+            ///Создаем поток для работы с записью в файл
+            thread_WR = new Thread(new ParameterizedThreadStart(WR_File.WriterInFILE));
+
+            ///Создаем поток для работы с UDP
+            thread_UDP = new Thread(new ThreadStart(UDPhandler.UDPRecive));
+
+        }
+
+        private void UDPhandler_DataRecive(byte[] vs)
+        {
+            try
+            {
+               if(thread_WR.IsAlive) thread_WR.Join();
+               else thread_WR.Start((object)vs);
+            }
+            catch(Exception e) { Console.WriteLine($"Исключение вида {e}"); }
+        }
+
+        public void StartRecive()
+        {
+            thread_UDP.Start();
+        }
+
+        /// <summary>
+        /// Метод полностью завершает выполнение потоков приема и записи данных
+        /// </summary>
+        public void Stop()
+        {
+            UDPhandler.UDPClose();
+            thread_WR.Abort();
+            thread_WR.Join();
+            thread_UDP.Abort();
+            thread_UDP.Join();
+        }
+    }
+}
